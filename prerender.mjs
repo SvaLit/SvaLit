e4f1e8d6540a06329494d7fc45ae572f6eace5c8
PonomareVlad <Git@PonomareVlad.ru>
2022-06-13 06:52:39 +0500

    Error handling and fixes for SSG

--- prerender.mjs
+++ prerender.mjs
@@ -1,4 +1,4 @@
-import {writeFile} from 'fs/promises'
+import {writeFile, mkdir} from 'fs/promises'
 import RenderThread from "./index.mjs"
 
 export const render = ({
@@ -14,7 +14,8 @@ export const render = ({
             ...renderOptions,
             ...options,
             meta: {url: new URL(path, origin)}
-        }).renderTemplate(tpl).then(html => writeFile(new URL(convertPath(path), publicDir), html)).then(() => path)))
+        }).renderTemplate(tpl).then((html, targetPath = new URL(convertPath(path), publicDir)) =>
+            mkdir(new URL('./', targetPath)).catch(e => console.error(e)).then(() => writeFile(targetPath, html).catch(e => console.error(e)))).then(() => path)))
 
 export const convertPath = path => (path.startsWith('/') ? '.' : '') +
     (path.endsWith('/') ? (path + 'index.html') : (path.endsWith('.html') ? path : (path + '/index.html')))
